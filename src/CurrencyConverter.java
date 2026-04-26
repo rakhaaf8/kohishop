67041ec6c1b7faca6d91bb059e52d92f3b93df42
Rakha Ahmad Fabiano <rakhafabiano87@gmail.com>
2026-04-26 18:40:38 +0700

    kohishop v4

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -4,21 +4,29 @@ public interface CurrencyConverter {
 }
 
 class USD implements CurrencyConverter {
+    @Override
     public double convertFromIdr(double idrAmount) { return idrAmount / 15.0; }
+    @Override
     public String getSymbol() { return "USD"; }
 }
 
 class JPY implements CurrencyConverter {
+    @Override
     public double convertFromIdr(double idrAmount) { return idrAmount * 10.0; }
+    @Override
     public String getSymbol() { return "JPY"; }
 }
 
 class MYR implements CurrencyConverter {
+    @Override
     public double convertFromIdr(double idrAmount) { return idrAmount / 4.0; }
+    @Override
     public String getSymbol() { return "MYR"; }
 }
 
 class EUR implements CurrencyConverter {
+    @Override
     public double convertFromIdr(double idrAmount) { return idrAmount / 14.0; }
+    @Override
     public String getSymbol() { return "EUR"; }
 }
